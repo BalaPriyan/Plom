3587748920c2b3070aa992445db9f3b4b836adbf
Shyamkumar04 <136441710+Shyamkumar04@users.noreply.github.com>
2023-09-28 11:45:23 +0530

    Update greetings.py

--- mbot/plugins/greetings.py
+++ mbot/plugins/greetings.py
@@ -49,7 +49,8 @@ async def start(client,message):
     if message.chat.type != "private" and message.chat.id not in AUTH_CHATS and message.from_user.id not in SUDO_USERS:
         return await message.reply_text("This Bot Will Not Work In Groups Unless It's Authorized.",
                     reply_markup=InlineKeyboardMarkup(reply_markup))
-    return await message.reply_text(f"Hello {message.from_user.first_name}, I'm a  Music Downloader Bot. /n Powered By MohanishX⚡",
+    return await message.reply_text(f"Hello {message.from_user.first_name}, I'm a  Music Downloader Bot.
+    Powered By MohanishX⚡",
                     reply_markup=InlineKeyboardMarkup(reply_markup))
 
 @Mbot.on_message(filters.command("restart") & filters.chat(OWNER_ID) & filters.private)
